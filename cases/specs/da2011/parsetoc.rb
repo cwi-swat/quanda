615dd28ab9416c6bd012eb8eeaa582d6cc164b79
Tijs van der Storm <T.van.der.Storm@cwi.nl>
2012-03-09 16:10:35 +0000

    Added talk for next Monday.

--- cases/specs/da2011/parsetoc.rb
+++ cases/specs/da2011/parsetoc.rb
@@ -2,13 +2,17 @@
 
 require 'rexml/document'
 require 'yaml'
+require 'pp'
+
+ENCODING = 'windows-1252'
 
 class TOC
   include REXML
 
   attr_reader :entries
 
-  def initialize
+  def initialize(body)
+    @body = body
     @entries = []
   end
 
@@ -23,25 +27,78 @@ class TOC
   def to_xml
     toc = YAML.load(to_yaml)
     root = Element.new('toc')
-    entries2xml(toc, root)
+    entries2xml(toc, root, titles = [])
     doc = Document.new
+    doc << XMLDecl.new('1.1', ENCODING)
     doc << root
+
+#     map = {}
+#     pp titles
+#     current = titles.shift
+#     prev = nil
+#     sect = false
+#     lines = @body.split(/\n/)
+#     puts lines.length
+#     len = lines.length
+#     i = 0
+
+#     cur = titles.shift
+#     i = search(cur)
+#     while i < len do
+#       prev = cur
+#       cur = titles.shift
+#       ids, i = collect_ids(i, cur)
+#       map[prev] = ids
+#     end
+#     while i < len do
+#       if current 
+#         if lines[i] =~ Regexp.new(Regexp.escape(current).encode(ENCODING)) then
+#           puts "CURRENT = #{current}"
+#           map[current] = []
+#           prev = current
+#           sect = true
+#           current = titles.shift
+#           i += 1
+#         end
+#         i += 1
+#       end
+#       if sect then
+#         while i < len do
+#           if lines[i] =~ /^Identificatienummer (\d+)$/
+#             map[prev] << $1
+#           end
+#           if lines[i] =~ Regexp.new(Regexp.escape(current).encode(ENCODING)) 
+#             sect = false
+#             break
+#           end
+#           i += 1
+#         end
+#         i += 1
+#       end
+#     end
+
+#     pp map
+
+
+
+    return doc
   end
 
   private
 
-  def entries2xml(es, elt)
+  def entries2xml(es, elt, titles)
     es.each do |hash|
-      elt << entry2xml(hash)
+      elt << entry2xml(hash, titles)
     end
   end
 
-  def entry2xml(hash)
+  def entry2xml(hash, titles)
+    titles << hash['title']
     elt = Element.new('entry')
     elt.attributes['title'] = hash['title']
     elt.attributes['page'] = hash['page']
     if hash["entries"] then
-      entries2xml(hash["entries"], elt)
+      entries2xml(hash["entries"], elt, titles)
     end
     return elt
   end
@@ -50,7 +107,8 @@ class TOC
     # NB: the truncation makes it not very precise
     # but it works because indents are different enough.
     mw = max_width.truncate
-    entries.each do |e|
+    len = entries.length
+    entries.each_with_index do |e, i|
       ew = e.width.truncate
       ind = ' ' * (mw - ew)
       title = e.title =~ /:/ ? "\"#{e.title}\"" : e.title
@@ -96,13 +154,14 @@ class TOCParser
 
   attr_reader :doc
 
-  def initialize(filename)
-    toc = File.read(ARGV[0], :encoding => ENCODING) 
+  def initialize(filename, body)
+    toc = File.read(filename, :encoding => ENCODING) 
+    @body = body
     @doc = Document.new(toc)
   end
 
   def toc
-    t = TOC.new
+    t = TOC.new(@body)
     doc.elements.each('pages/page/textbox/textline') do |x|
       chars = []
       x.elements.each('text') do |txt|
@@ -129,12 +188,15 @@ end
 
 
 if __FILE__ == $0 then
-  tp = TOCParser.new(ARGV[0])
+  text = File.read(ARGV[0], :encoding => ENCODING)
+  tp = TOCParser.new(ARGV[1], text)
   toc = tp.toc
   
   doc = toc.to_xml
   pp = REXML::Formatters::Pretty.new
-  pp.write(doc, $stdout)
+  File.open(ARGV[2], "w:#{ENCODING}") do |f|
+    pp.write(doc, f)
+  end
 end
 
 
