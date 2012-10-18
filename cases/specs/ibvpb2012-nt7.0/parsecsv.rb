af8a4e3cac50eb4eca476a97959ec411c2b07f79
Tijs van der Storm <storm@cwi.nl>
2012-10-18 14:21:42 +0200

    Updated scripts for 2012.

---
+++ cases/specs/ibvpb2012-nt7.0/parsecsv.rb
@@ -0,0 +1,299 @@
+
+require 'rexml/document'
+
+# CONFIG
+
+RIGHT_PAGE_REGEXP = /^Gegevensspecificatie DA-2011 - \(DA-2011\) (\d+)$/
+LEFT_PAGE_REGEXP = /^(\d+)$/
+NAME_REGEXP = /Naam :(( [^A(][A-Za-z.\-\/0-9]*)+|)/
+EXP_NAME_REGEXP = /Naam :( [a-zA-Z0-9]+)+/
+ENCODING = 'windows-1252'
+
+
+class Fixes
+
+  def fix_exp_119299(name, exp)
+    name == 'VPB einde boekjaar' ? '(' + exp : exp
+  end
+
+  def fix_exp_119363(name, exp)
+    name == 'IB einde boekjaar' ? '(' + exp : exp
+  end
+
+  def fix_cond_507322(name, cond)
+    name == '206' ? '(' + cond : cond
+  end
+
+  def fix_cond_512841(name, cond)
+    name == '261' ? cond.sub(/(premieplichtig\.\.Ja>)/, '\1);') : cond
+  end
+
+  def fix_cond_117838(name, cond)
+    name == '144' ? cond.sub(/\)$/, '') : cond
+  end
+
+  def fix_cond_117304(name, cond)
+    cond.sub(/(tingplichtig\.\.Ja>)/, '\1;')
+  end
+
+  def fix_cond_117305(name, cond)
+    cond.sub(/(tingplichtig\.\.Ja>\))/, '\1;')
+  end
+
+  def fix_cond_117308(name, cond)
+    cond.sub(/(partner\.\.Ja> \) \))$/, '\1)')
+  end
+
+end
+
+module TaxSpec
+
+  class Spec
+    include REXML
+    attr_reader :datums
+
+    def initialize(name, datums, text)
+      @name = name
+      @datums = datums
+      init_pages(text)
+    end
+
+    def to_s
+      datums.join("\n")
+    end
+
+    def to_dot(out = $stdout)
+      out << "digraph spec {\n"
+      datums.each do |datum|
+        out << "n#{datum.id};\n"
+      end
+      datums.each do |datum|
+        datum.relates_to.split(', ').each do |other|
+          out << "n#{datum.id} -> n#{other}\n"
+        end
+      end
+      out << "}\n"
+    end
+
+    
+    def to_xml
+      doc = Document.new
+      root = Element.new('tax-spec')
+      root.attributes['name'] = @name
+      datums.each do |datum|
+        elt = datum.to_xml
+        page = @pages[datum.id]
+        raise "No page for #{datum.id}" if page.nil?
+        elt.attributes['page'] = page
+        root << elt
+      end
+      doc << XMLDecl.new('1.1', ENCODING)
+      doc << root
+      return doc
+    end
+
+    def init_pages(text)
+      @pages = {}
+      page = nil
+      text.each_line do |line|
+        if line =~ LEFT_PAGE_REGEXP || line =~ RIGHT_PAGE_REGEXP then
+          page = $1
+        end
+        if line =~ /^Identificatienummer (\d+)$/
+          raise "No page for #{id}" if page.nil?
+          @pages[$1] = page
+        end
+      end
+    end
+
+  end
+  
+  class Datum
+    include REXML
+    attr_reader :name, :id, :definition, :explanation, :format
+    attr_reader :conditions, :domain, :relates_to, :source
+    attr_reader :expressions, :compute_as
+
+    @@fixes = Fixes.new
+
+    def initialize(name, id, definition, explanation, format, 
+                   conditions, domain, relates_to, source,
+                   expressions, compute_as)
+      @name = name
+      @id = id
+      @definition = definition
+      @explanation = explanation
+      @format = format
+      @conditions = conditions
+      @domain = domain
+      @relates_to = relates_to
+      @source = source
+      @expressions = expressions
+      @compute_as = compute_as
+    end
+    
+    def to_xml
+      elt = Element.new('datum')
+      elt.attributes['name'] = name
+      elt.attributes['id'] = id
+      elt.attributes['format'] = format
+      elt.attributes['domain'] = domain
+      elt.attributes['relates_to'] = relates_to
+      labels = [:definition, :explanation,:source, :compute_as]
+      labels.each do |label|
+        kid = Element.new(label.to_s)
+        kid.text = send(label)
+        elt << kid
+      end
+
+      # Conditions
+      kid = Element.new('conditions')
+      conditions.split("%%").each do |cond|
+        celt = Element.new('condition')
+        cond = cond.squeeze(' ')
+        if cond =~ NAME_REGEXP then
+          name = $1.strip
+          cond = cond.sub(NAME_REGEXP, '')
+          fix = "fix_cond_#{id}"
+          if @@fixes.respond_to?(fix) then
+            cond = @@fixes.send(fix, name, cond)
+          end
+          celt.attributes['name'] = name
+        else
+          celt.attributes['name'] = ''
+        end
+        celt.text = cond.sub(/\.$/, '')
+        kid << celt
+      end
+      elt << kid
+      
+      # Expressions
+      kid = Element.new('expressions')
+      re = /Naam :(?: [a-zA-Z0-9]+)+/
+      names = expressions.scan(re)
+      exps = expressions.split(re)
+      exps.shift 
+      if names.length != exps.length then
+        $stderr << "NAMES = #{names}\n"
+        $stderr << "EXPS = #{exps}\n"
+        $stderr << "\n\t#{expressions.inspect}\n"
+        raise "Names and exps are not compatible"
+      end
+      if names.length == 0 then
+        # EXP_NAME_REGEXP = /Naam :( [a-zA-Z0-9]+)+/
+        names = [''] # todo: depends on exp_name_regexp
+        exps = [expressions]
+      else
+        names.map! do |name|
+          if name =~ /Naam :((?: [a-zA-Z0-9]+)+)/ then
+            $1.strip
+          else
+            raise "Could not match name #{name}"
+          end
+        end
+      end
+      names.each_with_index do |name, i|
+        next if exps[i].empty?
+        exp = Element.new('expression')
+        exp.attributes['name'] = name if !name.empty?
+        x = exps[i]
+        fix = "fix_exp_#{id}"
+        if @@fixes.respond_to?(fix) then
+          x = @@fixes.send(fix, name, x)
+        end
+        exp.text = x.sub(/\.$/, '')
+        kid << exp
+      end
+      elt << kid
+      return elt
+    end
+
+    def to_s
+      values = [name, id, definition, explanation, format, 
+                conditions, domain, relates_to, source,
+                expressions, compute_as]
+      labels = [:name, :id, :definition, :explanation, :format,
+                :conditions, :domain, :relates_to, :source,
+                :expressions, :compute_as]
+      items = labels.zip(values).map do |label, value|
+        "#{label}: #{value}"
+      end
+      "{#{items.join(",\n")}}"
+    end
+  end
+  
+  class CSV
+    def self.read(file, text)
+      src = File.read(file, :encoding => ENCODING).encode('UTF-8')
+      datums = []
+      first = true
+      # start at 1 to skip column headers
+      src.each_line do |line|
+        if first then
+          first = false
+          next
+        end
+        elts = []
+        loop do
+          col, line = parse_value(line)
+          elts << col 
+          break unless line
+        end
+        datums << Datum.new(*elts)
+      end
+      Spec.new(File.basename(file), datums, text)
+    end
+    
+    
+    # Assumptions
+    #  - strings use " as quotes
+    #  - string can be nested (e.g. "'s should be balanced)
+    #  - embedded strings cannot have a comma right after the closing quote
+    def self.parse_string(line)
+      quotes = 0
+      val = ''
+      last_was_quote = false
+      pos = 0
+      line.each_char do |c|
+        if c == '"' then
+          quotes += 1
+          last_was_quote = true
+        elsif  c == ',' then
+          if last_was_quote && quotes % 2 == 0 then
+            break
+          end
+          last_was_quote = false
+        else
+          last_was_quote = false
+        end
+        pos += 1
+        val << c
+      end  
+      return val[1..-2], line[pos + 1..-1]
+    end
+    
+    def self.parse_value(line)
+      line = line.lstrip
+      if line =~ /^"/
+        return parse_string(line)
+      else
+        # the next comma is the delimiter
+        if line =~ /^(.*?),(.*)$/ then
+          return $1, $2
+        end
+        return line, nil
+      end
+    end
+  end
+end
+
+
+
+if __FILE__ == $0 then
+  text = File.read(ARGV[0], :encoding => ENCODING)
+  spec = TaxSpec::CSV.read(ARGV[1], text)
+  pp = REXML::Formatters::Pretty.new
+  File.open(ARGV[2], "w:#{ENCODING}") do |f|
+    pp.write(spec.to_xml, f)
+  end
+end  
---
+++ cases/specs/ibvpb2012-nt7.0/parsetoc.rb
@@ -0,0 +1,205 @@
+
+
+require 'rexml/document'
+require 'yaml'
+require 'pp'
+
+ENCODING = 'windows-1252'
+
+class TOC
+  include REXML
+
+  attr_reader :entries
+
+  def initialize(body)
+    @body = body
+    @entries = []
+  end
+
+  def max_width
+    entries.map(&:width).max || 0
+  end
+
+  def <<(entry)
+    entries << entry
+  end
+
+  def to_xml
+    toc = YAML.load(to_yaml)
+    root = Element.new('toc')
+    entries2xml(toc, root, titles = [])
+    doc = Document.new
+    doc << XMLDecl.new('1.1', ENCODING)
+    doc << root
+
+#     map = {}
+#     pp titles
+#     current = titles.shift
+#     prev = nil
+#     sect = false
+#     lines = @body.split(/\n/)
+#     puts lines.length
+#     len = lines.length
+#     i = 0
+
+#     cur = titles.shift
+#     i = search(cur)
+#     while i < len do
+#       prev = cur
+#       cur = titles.shift
+#       ids, i = collect_ids(i, cur)
+#       map[prev] = ids
+#     end
+#     while i < len do
+#       if current 
+#         if lines[i] =~ Regexp.new(Regexp.escape(current).encode(ENCODING)) then
+#           puts "CURRENT = #{current}"
+#           map[current] = []
+#           prev = current
+#           sect = true
+#           current = titles.shift
+#           i += 1
+#         end
+#         i += 1
+#       end
+#       if sect then
+#         while i < len do
+#           if lines[i] =~ /^Identificatienummer (\d+)$/
+#             map[prev] << $1
+#           end
+#           if lines[i] =~ Regexp.new(Regexp.escape(current).encode(ENCODING)) 
+#             sect = false
+#             break
+#           end
+#           i += 1
+#         end
+#         i += 1
+#       end
+#     end
+
+#     pp map
+
+
+
+    return doc
+  end
+
+  private
+
+  def entries2xml(es, elt, titles)
+    es.each do |hash|
+      elt << entry2xml(hash, titles)
+    end
+  end
+
+  def entry2xml(hash, titles)
+    titles << hash['title']
+    elt = Element.new('entry')
+    elt.attributes['title'] = hash['title']
+    elt.attributes['page'] = hash['page']
+    if hash["entries"] then
+      entries2xml(hash["entries"], elt, titles)
+    end
+    return elt
+  end
+
+  def to_yaml(out = '')
+    # NB: the truncation makes it not very precise
+    # but it works because indents are different enough.
+    mw = max_width.truncate
+    len = entries.length
+    entries.each_with_index do |e, i|
+      ew = e.width.truncate
+      ind = ' ' * (mw - ew)
+      title = e.title =~ /:/ ? "\"#{e.title}\"" : e.title
+      out << "#{ind}- title: #{title}\n"
+      out << "#{ind}  page: #{e.page}\n"
+      out << "#{ind}  entries: \n"
+    end
+    return out.empty? ? "[]" : out
+  end
+
+end
+
+class TOCEntry
+  attr_reader :width, :title, :page
+
+  def initialize(width, title, page)
+    @width = width
+    @title = title
+    @page = page
+  end
+  
+  def to_s
+    "#{width}: #{title} : #{page}"
+  end
+end
+
+class TOCParser
+  include REXML
+
+  # Config
+  ENCODING = 'UTF-8'
+  LIT_SKIP = ['20/6/2012', 'Gegevensspecificatie DA-2012 - (DA-2012 IHZ-VPB)']
+  REG_SKIP = [/^\d+$/, # page number
+              /^Toelichting behorende/, 
+              /^Index/, 
+              /^Inhoudsopgave/,
+              /^Gehanteerde/, 
+              /^Inkomsten- vennoot/]
+
+  SKIP = REG_SKIP + LIT_SKIP.map { |x| 
+    Regexp.new('^' + Regexp.escape(x) + '$') 
+  }
+
+  attr_reader :doc
+
+  def initialize(filename, body)
+    toc = File.read(filename, :encoding => ENCODING) 
+    @body = body
+    @doc = Document.new(toc)
+  end
+
+  def toc
+    t = TOC.new(@body)
+    doc.elements.each('pages/page/textbox/textline') do |x|
+      chars = []
+      x.elements.each('text') do |txt|
+        if txt.attributes['bbox'] then
+          chars << txt.text
+        end
+      end
+
+      line = chars.join
+      elts = line.split(/\.+/)
+      if elts.length > 2 then
+        puts "ELTS = #{elts.inspect}"
+        #raise "Invalid toc line"
+      end
+      title, page = elts
+      next if SKIP.any? { |re| re =~ title }
+      
+      bbox = x.attributes['bbox']
+      lux, luy, rlx, rly = bbox.split(',').map(&:to_f)
+      width = rlx - lux
+      
+      t << TOCEntry.new(width, title, page)
+    end
+    return t
+  end
+end
+
+
+if __FILE__ == $0 then
+  text = File.read(ARGV[0], :encoding => ENCODING)
+  tp = TOCParser.new(ARGV[1], text)
+  toc = tp.toc
+  
+  doc = toc.to_xml
+  pp = REXML::Formatters::Pretty.new
+  File.open(ARGV[2], "w:#{ENCODING}") do |f|
+    pp.write(doc, f)
+  end
+end
+
+
